e4c177815bb2c31d4479edbf3626abe4f01e44a6
Gerardo <ferrari_gerardo@live.com.mx>
2018-06-20 04:38:57 +0200

    Primer Script
    Clase 1

---
+++ clase1.1.py
@@ -0,0 +1,96 @@
+print("\t\tHola mundo\n\n")
+print("Este es otro mensaje")
+
+#Tipos de Dato
+#Tipado Dinamico
+
+#Entero
+variable = 3 
+# Real, Flotante o Doble 
+variable2 = 3.57676
+#Booleano
+variableBooleana = True
+variableBooleanaFalse = False
+#Cadenas
+variableString = "Esto es una cadena"
+
+#Imprimir 
+print(variable2)
+print(variable)
+print(variableBooleana)
+print(variableBooleanaFalse)
+print(variableString)
+
+print(type(variable))
+print(type(variable2))
+print(type(variableBooleana))
+print(type(variableString))
+
+#Operaciones Aritmeticas 
+numero1 = 10
+numero2 = 3
+#Suma
+print(numero1 + numero2)
+#Resta
+print(numero1 - numero2)
+#Multiplicación 
+print(numero1 * numero2)
+#Potencia 
+print(numero1 ** numero2)
+#Division 
+print(numero1 / float(numero2))
+
+#Comparadores.
+print(3 < 5) # True
+print(8 < 4) # False
+print(10 > 6) # True
+print(5 > 10) # False
+print(5 == 5) # True
+print(5 != 6) # True
+
+print("Hola" == "hola")
+
+#Sentencias de control.
+numero1 = 17
+numero2 = 10
+if(int(numero1) > numero2):
+	print("El"+ str(numero1) + "es mayor")
+else:
+	print("El ",numero2 ,"numero es mayor a 7")
+## Persona > 18 , Persona == 18, Persona < 18
+Persona = 20
+if(Persona > 18) :
+	print("La persona es mayor de edad")
+elif(Persona == 18):
+	print("Pedir el INE")
+elif():
+	
+else:
+	print("Menor de edad")
+
+if(Persona > 18):
+	print("Mayor de edad")
+if(Persona == 18):
+	print("Tiene 18 años")
+if(Persona < 18):
+	print("Es menor de edad")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
